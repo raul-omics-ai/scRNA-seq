85921a795b45a1003d702dc71e1338469c3c52fe
Raúl Fernández Contreras <raulfcontreras97@gmail.com>
2025-11-17 09:51:10 +0100

    Update harmony integration script source path

--- 02_Downstream_Analysis/ultimate_scrnaseq_analysis_v5.R
+++ 02_Downstream_Analysis/ultimate_scrnaseq_analysis_v5.R
@@ -46,7 +46,7 @@ automatic_scrnaseq_analysis <- function(list_scr,
   source("~/Documentos/09_scripts_R/automatic_QC_scrnaseq.R")
   source("~/Documentos/09_scripts_R/automate_standard_seurat_processing.R")
   source("~/Documentos/09_scripts_R/automatic_doublet_detection.R")
-  source("~/Documentos/09_scripts_R/harmony_automate_integration_v1.R")
+  source("~/Documentos/09_scripts_R/harmony_automate_integration.R")
   source("~/Documentos/09_scripts_R/automatic_clustering_scrnaseq_v1.R")
   
   source("~/Documentos/09_scripts_R/print_centered_note_v1.R")
